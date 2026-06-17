9f91bb96f3a443f81abc0fd8071e1f14e6523826
fabianpallero <37967560+fabianpallero@users.noreply.github.com>
2026-06-17 19:51:32 -0400

    Add pass/fail message based on final grade

--- Registro_de_notas.py
+++ Registro_de_notas.py
@@ -3,3 +3,8 @@ nombre = input("Ingrese nombre del estudiante: ")
 nota = float(input("Ingrese nota final: "))
 print("Estudiante:", nombre)
 print("Nota final:", nota)
+
+if nota <= 4.0:
+  print("Aprobado
+else:
+  print("Reprobado")
